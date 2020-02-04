487db9a42127b6c7b6672b4dab2ac3b9fa94160a
Praveen Bisht <prvnbist@gmail.com>
2020-02-04 11:38:34 +0530

    feat: sidebar list

--- src/sections/Sidebar/index.jsx
+++ src/sections/Sidebar/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react'
 
 // Styled
-import { StyledSidebar } from "./styled";
+import {
+   StyledSidebar,
+   StyledList,
+   StyledListItem,
+   StyledHeading,
+} from './styled'
 
 const Sidebar = ({ visible }) => {
-  return <StyledSidebar visible={visible}>Sidebar</StyledSidebar>;
-};
+   return (
+      <StyledSidebar visible={visible}>
+         <StyledHeading>Listings</StyledHeading>
+         <StyledList>
+            <StyledListItem>Users</StyledListItem>
+            <StyledListItem>Devices</StyledListItem>
+            <StyledListItem>Roles</StyledListItem>
+            <StyledListItem>Apps</StyledListItem>
+         </StyledList>
+      </StyledSidebar>
+   )
+}
 
-export default Sidebar;
+export default Sidebar
