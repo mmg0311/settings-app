5ad178e80c9e2930afc17b176442e0450a1bcc17
Praveen Bisht <prvnbist@gmail.com>
2020-03-13 14:11:27 +0530

    feat: apollo client integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Apollo Client Imports
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+
 import App from './App'
 
 import { TabProvider } from './store/tabs'
 
 import './index.css'
 
+const client = new ApolloClient({
+   link: ApolloLink.from([
+      new HttpLink({
+         uri: process.env.REACT_APP_GRAPHQL_URI,
+      }),
+   ]),
+   cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
-   <TabProvider>
-      <App />
-   </TabProvider>,
+   <ApolloProvider client={client}>
+      <TabProvider>
+         <App />
+      </TabProvider>
+   </ApolloProvider>,
    document.getElementById('root')
 )
