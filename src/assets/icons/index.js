60a8dfa2ff6fc48bc121c104601258773372bcda
Praveen Bisht <prvnbist@gmail.com>
2020-03-13 13:53:12 +0530

    feat: prev/next page nav

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,15 @@ import CloseIcon from './Close'
 import EditIcon from './Edit'
 import DeleteIcon from './Delete'
 import AddIcon from './Add'
+import LeftIcon from './Left'
+import RightIcon from './Right'
 
-export { MenuIcon, CloseIcon, EditIcon, DeleteIcon, AddIcon }
+export {
+   MenuIcon,
+   CloseIcon,
+   EditIcon,
+   DeleteIcon,
+   AddIcon,
+   LeftIcon,
+   RightIcon,
+}
